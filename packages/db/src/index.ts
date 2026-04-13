82cc83c6f019107219e3eaab27da43f9074ae3fa
fgbyte <fer.garciabayon@gmail.com>
2026-04-13 12:54:28 -0400

    Use direct connection for db client
    This change reverts the database client initialization to use a direct
    connection instead of a connection pool. This is intended for local
    development environments.

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -6,3 +6,15 @@ import * as schema from "./schema";
 
 const sql = neon(env.DATABASE_URL || "");
 export const db = drizzle(sql, { schema, casing: "snake_case" });
+
+// For only local dev use 👇
+// import { env } from "@todo-hono-postmark/env/server";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import * as schema from "./schema";
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//   connectionString: env.DATABASE_URL,
+// });
+
+// export const db = drizzle(pool, { schema, casing: "snake_case" });
