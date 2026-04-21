7f456dd537e46dd822565a8f104819be5c5a2a0c
fgbyte <fer.garciabayon@gmail.com>
2026-04-21 00:37:30 -0400

    Add PATCH and DELETE to CORS allowed methods

--- apps/server/src/middlewares/cors-middleware.ts
+++ apps/server/src/middlewares/cors-middleware.ts
@@ -3,7 +3,7 @@ import { env } from "@todo-hono-postmark/env/server";
 
 export const corsMiddleware = cors({
   origin: env.CORS_ORIGIN, //alchemy envs
-  allowMethods: ["GET", "POST", "OPTIONS"],
+  allowMethods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   allowHeaders: ["Content-Type", "Authorization"],
   credentials: true,
   exposeHeaders: ["Set-Cookie", "Content-Length"],
